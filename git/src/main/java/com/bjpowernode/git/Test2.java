bee4ff1b7cb14cbc692b78adf074cf518fad9859
wangming <13521594861@139.com>
2022-07-15 16:23:56 +0800

    提交更新

---
+++ git/src/main/java/com/bjpowernode/git/Test2.java
@@ -0,0 +1,11 @@
+package com.bjpowernode.git;
+
+/**
+ * @author wangjingyu
+ * @create 2022/7/15
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("=======hello world2=======");
+    }
+}
